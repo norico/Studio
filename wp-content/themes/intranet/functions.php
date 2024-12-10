481f26d357828d84bffa630263670ac1642a9c4a
norico <legion.norico@gmail.com>
2024-12-10 21:24:46 +0100

    theme 1.0.1

--- wp-content/themes/intranet/functions.php
+++ wp-content/themes/intranet/functions.php
@@ -6,5 +6,9 @@ add_action('wp_enqueue_scripts', 'intranet_enqueue_scripts');
 
 
 function intranet_enqueue_scripts() {
-	wp_enqueue_style('intranet-style', get_stylesheet_directory_uri().'/assets/css/style.css', [], '1.0.0', 'screen');
+
+	$theme_name = wp_get_theme()->get( 'Name' );
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style($theme_name.'-style', get_stylesheet_directory_uri().'/assets/css/style.css', [], $theme_version, 'screen');
 }
