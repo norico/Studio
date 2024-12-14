0a8e93188b0abff97e4fd0a2c7163bcd4629db17
norico <legion.norico@gmail.com>
2024-12-15 00:15:52 +0100

    post image

--- wp-content/themes/intranet/template-parts/content-post.php
+++ wp-content/themes/intranet/template-parts/content-post.php
@@ -1,13 +1,10 @@
 <article <?php post_class(); ?> >
 	<?php
-
-	post_image(get_the_ID(), true, null );
-
+	post_image();
 	if ( is_single() ) :
 	the_title( '<h1 class="entry-title">', '</h1>' );
 	else :
 		printf('<p><a href="%s"><h1 class="entry-title">%s</h1></a></p>',  get_the_permalink(), get_the_title());
 	endif;
 	?>
-
 </article>
